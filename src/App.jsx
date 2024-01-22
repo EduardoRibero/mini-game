f2c752c9e7ccd81b6189397fe5392adb31266f13
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-01-22 15:13:34 -0300

    Alterando os estágios do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import { wordsList } from './data/Words';
 
 // COMPONENTES
 import StartScreen from './Componentes/StartScreen';
+import Game from './Componentes/Game';
+import GameOver from './Componentes/GameOver';
 
 const stages = [
   {id: 1, name: "start"},
@@ -22,12 +24,24 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  const restart = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
       {/*Renderização condicional, se o 'gameStage' for igual a 'start' ele exibe o componente <StartScreen/>*/}
-        {gameStage === "start" && <StartScreen/>} 
-        {gameStage === "game" && <StartScreen/>} 
-        {gameStage === "end" && <StartScreen/>} 
+        {gameStage === "start" && <StartScreen startGame={startGame}/>} 
+        {gameStage === "game" && <Game verifyLetter={verifyLetter}/>} 
+        {gameStage === "end" && <GameOver restart={restart}/>} 
     </div>
   );
 }
--- src/Componentes/GameOver.jsx
+++ src/Componentes/GameOver.jsx
@@ -1,9 +1,10 @@
 import "./GameOver.css"
 
-const GameOver = () => {
+const GameOver = ({restart}) => {
   return (
     <div>
-        
+        <h1>Game Over</h1>
+        <button onClick={restart}>Reiniciar o jogo</button>
     </div>
   )
 }
