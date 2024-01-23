4240398d89db3fc728595df00ea0e25010f7f7b0
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-01-22 23:29:48 -0300

    Componente Game

--- src/Componentes/Game.jsx
+++ src/Componentes/Game.jsx
@@ -2,9 +2,38 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalizar o jogo</button>
+    <div className="game">
+
+      <p className="points">
+          <span>Pontuação: 000</span>
+      </p>
+
+      <h1>Advinhe a palavra:</h1>
+
+      <h3 className="tip">
+          Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className="wordContainer">
+          <span className="letter">a</span>
+          <span className="blankSquare"></span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+
+      <div className="wrongLetterContainer">
+        <p>
+          Letras ja utilizadas:
+        </p>
+        <span>A,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
